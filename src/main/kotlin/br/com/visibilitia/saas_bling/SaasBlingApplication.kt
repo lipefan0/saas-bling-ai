a24054a18ddd85c3668483d63b6f30942d803a0b
lipefan0 <lipefan0@gmail.com>
2025-11-10 17:31:27 -0300

    initial commit

---
+++ src/main/kotlin/br/com/visibilitia/saas_bling/SaasBlingApplication.kt
@@ -0,0 +1,11 @@
+package br.com.visibilitia.saas_bling
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SaasBlingApplication
+
+fun main(args: Array<String>) {
+	runApplication<SaasBlingApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/visibilitia/saas_bling/SaasBlingApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.visibilitia.saas_bling
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SaasBlingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
